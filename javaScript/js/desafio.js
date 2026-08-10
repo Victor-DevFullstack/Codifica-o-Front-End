25e5c136ac7d8de0ace42f85db8ebf7de1dc990c
Victor André Lima Mourão de Oliveira <elane.asfunix99@email.com>
2026-08-10 17:02:07 -0300

    fead

---
+++ javaScript/js/desafio.js
@@ -0,0 +1,45 @@
+import {imagem1} from ""
+
+const produtos = [
+    {
+        id:1,
+        nome:"Casaco branco",
+        preço: 289,90,
+        categoria:"Roupas",
+        imagem:
+    }
+    {
+        id:2,
+        nome:"Roupa de cama",
+        preço: 490,90,
+        categoria:"Casa",
+        imagem: 
+    }
+    {
+        id:3,
+        nome: ,
+        preço:,
+        categoria:,
+        imagem:
+    }
+    {
+        id:4,
+        nome: ,
+        preço:,
+        categoria:,
+        imagem:
+    }
+    {
+        id:5,
+        nome:"Camisa preta de corrida",
+        preço: 159,99,
+        categoria:,
+        imagem:
+    }
+];
+
+function listarProdutos(lista {
+    lista.forEach(produto => {
+        const {id,nome,preço,produto,imagem}
+    })
+})
