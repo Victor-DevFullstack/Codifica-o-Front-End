a24642276fbcfe53d4814ef84c688e212fd95a44
Victor André Lima Mourão de Oliveira <elane.asfunix99@email.com>
2026-08-21 16:49:24 -0300

    feat(21/08)

---
+++ Angular/primeiro-projeto/src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -0,0 +1,34 @@
+import { Component, signal, computed} from '@angular/core';
+import { Produto } from '../produto/produto';
+
+@Component({
+  selector: 'app-lista-produtos',
+  imports: [Produto],
+  templateUrl: './lista-produtos.html',
+  styleUrl: './lista-produtos.css',
+})
+export class ListaProdutos {
+  produtos = signal([
+    {nome: "Notebook", preco: 3989},
+    {nome: "Mouse", preco: 179}
+  ]);
+
+  totalProdutos = computed(() => this.produtos().length);
+
+  valorTotal = computed(() => {
+    return this.produtos().reduce((total, item) => total + item.preco, 0);
+  });
+  exibirProduto(nome:string){
+    console.log(`Produto selecionado: ${nome}`)
+  }
+
+  adicionarProduto(){
+    this.produtos.update(listaAtual => 
+    [... listaAtual,{nome:'Teclado', preco:250}]
+    );
+  }
+
+  substituirProdutos(){
+    this.produtos.set([{nome:'Produto Novo', preco:1989}])
+  }
+}
